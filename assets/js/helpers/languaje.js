5298bde00252209977bda7bb7030bdc89c19160e
CATEHE <93102782+CATEHE@users.noreply.github.com>
2024-10-16 09:10:30 -0600

    Update languaje.js
    Se actualizo la funcionalidad de idioma

--- assets/js/helpers/languaje.js
+++ assets/js/helpers/languaje.js
@@ -7,11 +7,9 @@ function idioma () {
 
   if (check2.checked == true){
     location.href="index-en.html";
-    window.alert('pagina en ingles');
 
   } else {
     location.href="index.html";
-    window.alert('pagina en español');
 
   }
 }
